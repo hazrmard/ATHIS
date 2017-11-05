b6c9d017b13abdedb60f243e04e732219ba6727d
hazrmard <ibrahim78786@gmail.com>
2017-11-05 14:18:27 -0600

    Started C project

---
+++ C/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 5, 2017
+ *      Author: Ibrahim Ahmed
+ */
+
+
+#include <stdio.h>
+#include <pthread.h>
+
+int main(void) {
+	printf("Hello\n");
+	return 0;
+}
